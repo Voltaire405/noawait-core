8c652a19464759b30b2ddc03e708363b66b08b9d
Jose Marin <jlma.ing@protonmail.com>
2022-06-14 03:27:31 -0500

    first commit

---
+++ src/main/java/co/edu/udea/noawait/nowaitcore/NowaitCoreApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.noawait.nowaitcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowaitCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowaitCoreApplication.class, args);
+	}
+
+}
